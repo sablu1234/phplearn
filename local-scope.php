d0a7be1d335f2cf70053a457489f5155cfd4cde6
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-07-01 14:43:23 +0600

    frist code

---
+++ local-scope.php
@@ -0,0 +1,15 @@
+<?php
+ //global scope
+$persone_fb="fb/hadijaman";
+
+// function personeaddress
+// person_name;
+
+function getAddress(){
+    $person_name= "hadijaman";
+    echo $person_name;
+}
+getAddress();
+
+
+?>
